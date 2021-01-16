666cf89d8524fa9ba92c7cc09a1a77b3b56267d6
Benjamin DiGregorio <ben@digregor.io>
2021-01-15 20:11:06 -0800

    Add ability to rotate camera

---
+++ Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour {
+    private float horizontalInput;
+    private float rotationSpeed = 45;
+
+    private void Update() {
+        horizontalInput = Input.GetAxis("Horizontal");
+        float yRotation = horizontalInput * rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up, yRotation);
+    }
+}
