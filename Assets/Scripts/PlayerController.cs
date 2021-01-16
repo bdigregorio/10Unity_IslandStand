626f1f23cf62adb342d9e771f75143df4850b1c6
Benjamin DiGregorio <ben@digregor.io>
2021-01-16 13:58:37 -0800

    Add powerup indicator on player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,11 @@ public class PlayerController : MonoBehaviour {
 
     private Rigidbody thisRigidBody;
     private GameObject focalPointObject;
+    public GameObject powerupIndicator;
     public float speed;
-    public float powerUpStrength;
+    public float powerupStrength;
     private bool isPoweredUp = false;
-    public float powerUpDuration;
+    public float powerupDuration;
 
     private void Start() {
         InitializeComponents();
@@ -21,7 +22,7 @@ public class PlayerController : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Powerup")) {
-            ConsumePowerUp(other.gameObject);
+            ConsumePowerup(other.gameObject);
         }
     }
 
@@ -40,17 +41,20 @@ public class PlayerController : MonoBehaviour {
         // input value is between 0 and 1 
         float verticalInputMagnitude = Input.GetAxis("Vertical");
         thisRigidBody.AddForce(focalPointObject.transform.forward * verticalInputMagnitude * speed);
+        powerupIndicator.transform.position = transform.position + new Vector3(0f, -0.25f, 0f);
     }
 
-    private void ConsumePowerUp(GameObject powerUp) {
+    private void ConsumePowerup(GameObject powerup) {
         isPoweredUp = true;
-        Destroy(powerUp);
+        powerupIndicator.SetActive(true);
+        Destroy(powerup);
         StartCoroutine(PowerupCountdownRoutine());
     }
 
     IEnumerator PowerupCountdownRoutine() {
-        yield return new WaitForSeconds(powerUpDuration);
+        yield return new WaitForSeconds(powerupDuration);
         isPoweredUp = false;
+        powerupIndicator.SetActive(false);
         Debug.Log($"Powerup timer expired - isPoweredUp: {isPoweredUp}");
     }
 
@@ -61,6 +65,6 @@ public class PlayerController : MonoBehaviour {
         Debug.Log($"Player collided with ${enemy} with powerup set to ${isPoweredUp}");
         
         Vector3 awayFromPlayer = (enemy.transform.position - transform.position).normalized;
-        enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+        enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
     }
 }
