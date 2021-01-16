524be759397c95346984bf32d1fc46cc2213dd88
Benjamin DiGregorio <ben@digregor.io>
2021-01-15 20:29:13 -0800

    Add player controller to apply force to player based on input

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody playerRigidBody;
+    private GameObject focalPoint;
+    private float speed = 5;
+    private float verticalInput = 0;
+
+    void Start() {
+        playerRigidBody = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update() {
+        verticalInput = Input.GetAxis("Vertical");
+        playerRigidBody.AddForce(focalPoint.transform.forward * speed * verticalInput);
+    }
+}
